d8afbaec4a8afb209cea63fbfdc8715dfecc7e87
Tunahan SARI <tunahansari@hotmail.com>
2023-05-31 22:31:06 +0300

    End of section 9

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,13 @@
-import { Button, Card, Image } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { observer } from "mobx-react-lite";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import ActivityDetailedHeader from "./ActivityDetailedHeader";
+import ActivityDetailedInfo from "./ActivityDetailedInfo";
+import ActivityDetailedChat from "./ActivityDetailedChat";
+import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
 
 const ActivityDetails = () => {
     const {activityStore} = useStore();
@@ -18,22 +22,16 @@ const ActivityDetails = () => {
 
     if(loadingInitial || !selectedActivity) return <LoadingComponent/>; //This should not happen since we're not displaying this comp. without a selected activity
     
-    return <Card fluid>
-        <Image src={`/assets/categoryImages/${selectedActivity.category}.jpg`}/>
-        <Card.Content>
-            <Card.Header>{selectedActivity.title}</Card.Header>
-            <Card.Meta>
-                <span>{selectedActivity.date}</span>
-            </Card.Meta>
-            <Card.Description>{selectedActivity.description}</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-            <Button.Group widths={2}>
-                <Button as={Link} to={`/manage/${id}`} basic color="blue" content="edit" />
-                <Button as={Link} to='/activities' basic color="grey" content="cancel"/>
-            </Button.Group>
-        </Card.Content>
-    </Card>
+    return <Grid>
+        <Grid.Column width={10}>
+            <ActivityDetailedHeader activity={selectedActivity}/>
+            <ActivityDetailedInfo activity={selectedActivity}/>
+            <ActivityDetailedChat/>
+        </Grid.Column>
+        <Grid.Column width={6}>
+            <ActivityDetailedSidebar/>
+        </Grid.Column>
+    </Grid>
 }
 
 export default observer(ActivityDetails);
